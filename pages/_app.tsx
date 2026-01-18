7b79c9e26fbf0d684a80cf02ad27b4734726dcbd
English Learner <apps.manage8@gmail.com>
2026-01-18 18:54:14 +0900

    Revert "Add mobile and PWA support for Twitter Translator"
    This reverts commit 23e0cc70ba262ffe069605db84141421555c2510.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,43 +1,6 @@
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import { useEffect } from 'react'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-          .then((registration) => {
-            console.log('Service Worker registered:', registration);
-          })
-          .catch((error) => {
-            console.log('Service Worker registration failed:', error);
-          });
-      });
-    }
-  }, []);
-
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#1DA1F2" />
-        <meta name="description" content="Translate tweets from users you follow into English" />
-        
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icon16.png" />
-        <link rel="icon" type="image/png" sizes="48x48" href="/icon48.png" />
-        <link rel="apple-touch-icon" href="/icon128.png" />
-        
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Tweet Translate" />
-        
-        <title>Twitter Translator</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
--- pages/api/tweets.ts
+++ pages/api/tweets.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getFollowingTweets, getSingleTweet } from '../../lib/twitter';
-import { translateMultiple, translateText } from '../../lib/translator';
+import { getFollowingTweets } from '../../lib/twitter';
+import { translateMultiple } from '../../lib/translator';
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,34 +10,8 @@ export default async function handler(
     return res.status(405).json({ message: 'Method not allowed' });
   }
 
-  const { userId, tweetId, maxTweets = 20 } = req.query;
+  const { userId, maxTweets = 20 } = req.query;
 
-  // Single tweet mode
-  if (tweetId && typeof tweetId === 'string') {
-    try {
-      const tweet = await getSingleTweet(tweetId);
-      const translation = await translateText(tweet.text);
-      
-      const translatedTweet = {
-        ...tweet,
-        translation
-      };
-
-      res.status(200).json({
-        success: true,
-        tweets: [translatedTweet]
-      });
-    } catch (error) {
-      console.error('API Error:', error);
-      res.status(500).json({
-        success: false,
-        message: 'Failed to fetch and translate tweet'
-      });
-    }
-    return;
-  }
-
-  // User timeline mode
   if (!userId || typeof userId !== 'string') {
     return res.status(400).json({ message: 'User ID is required' });
   }
